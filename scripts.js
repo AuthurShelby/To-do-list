790dfc404e77c2b58061c3c7431dc2e9de0be402
Devanand Puzhakkool <132560246+AuthurShelby@users.noreply.github.com>
2024-11-18 20:11:48 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,46 @@
+const AddTask = document.getElementById('addtask');
+const AddTaskBtn = document.getElementById('AddEventButton');
+const GetItem = document.getElementById('get-item');
+const ErrorMsg = document.getElementById('error-msg');
+
+AddTaskBtn.addEventListener('click',function(){
+    AddingTask();
+    localStorage.getItem('tasks');
+});
+
+function AddingTask(){
+    const task = document.createElement('p');
+    task.textContent=AddTask.value.trim();
+    
+    if (AddTask.value.trim()==="")
+    {
+        EnteredValueNull();
+    }
+    else
+    {
+        task.classList.add('NewList');
+        GetItem.appendChild(task);
+        AddTask.value = "";
+        ErrorMsg.textContent="";
+    }
+
+    task.addEventListener('click',function(){
+        task.style.textDecoration="line-through";
+        task.style.backgroundColor="#17B169";
+        task.style.color = 'black';
+    });
+    task.addEventListener('dblclick',function(){
+        GetItem.removeChild(task);
+    });
+
+}
+function EnteredValueNull(){
+    AddTask.value="";
+    // AddTask.focus();
+    ErrorMsg.textContent="Task Adikkeda Kunne";
+}
+function KeyEnter(event){
+    if(event.key==="Enter"){
+        AddingTask();
+    }    
+}
